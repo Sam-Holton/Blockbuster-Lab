2a6780aa50595a2f524024d3e57fbc02b48e8dce
Sam-Holton <samuel.holton@gmail.com>
2020-12-16 21:41:38 -0500

    Work 121620

--- Blockbuster Lab/Program.cs
+++ Blockbuster Lab/Program.cs
@@ -7,113 +7,11 @@ namespace Blockbuster_Lab
     {
         static void Main(string[] args)
         {
-            
+            var Block
         }
     }
 
-    public class Blockbuster
-    {
-        public List<Movie> MovieList()
-        {
-            List<Movie> movies = new List<Movie>
-            {
-                new VHS
-                {
-                    Title = "The Fifth Element",
-                    RunTime = 127,
-                    Category = Genre.Action,
-                    Scenes =
-                    {
-                        "LeeLou gets blown Up",
-                        "LeeLou gets reconstructed",
-                        "LeeLou blows up bad planet",
-                        "LeeLou hooks up below her station"
-                    }
-                },
-                new VHS
-                {
-                    Title = "The Wizard of Oz",
-                    RunTime = 112,
-                    Category = Genre.Drama,
-                    Scenes =
-                    {
-                        "Dorothy goes to Oz",
-                        "Dorothy meets some odd folks",
-                        "Dorothy kills a woman",
-                        "Dorothy absconds back to Earth"
-                    }
-                },
-                new VHS
-                {
-                    Title = "Star Wars - A New Hope",
-                    RunTime = 125,
-                    Category = Genre.Action,
-                    Scenes =
-                    {
-                        "Vader tries to kill Leia",
-                        "Luke discovers he's not just a farmer's kid",
-                        "Luke levels up with Yoda",
-                        "Luke blows up Vader's toy"
-                    }
-                }, 
-                new DVD
-                {
-                    Title = "Inception",
-                    RunTime = 162,
-                    Category = Genre.Drama,
-                    Scenes =
-                    {
-                        "Cobb and crew hacks somebodies brain",
-                        "Cobb goes deeper into the brain",
-                        "Cobb goes deeper into the brain... again",
-                        "Cobb goes super deep and comes back... maybe"
-                    }
-                },
-                new DVD
-                {
-                    Title = "Alita, Battle Angel",
-                    RunTime = 122,
-                    Category = Genre.Drama,
-                    Scenes =
-                    {
-                        "Doc finds cyborg core in dump",
-                        "Doc rebuilds Alita with spare parts",
-                        "Alita trades up for better body",
-                        "Alita joins ultraviolent racing for chance to kill someone who doesn't race"
-                    }
-                },
-                new DVD
-                {
-                    Title = "Ender's Game",
-                    RunTime = 114,
-                    Category = Genre.Drama,
-                    Scenes =
-                    {
-                        "Ender gets recruited",
-                        "Ender leads teams to wins",
-                        "Ender forms an elite team for a different game",
-                        "Ender unknowingly commits genocide"
-                    }
-                }
-            };
-
-
-
-
-
-            return movies;
-        }
-
-        public void PrintMovies()
-        {
-
-        }
-
-        public void CheckOut()
-        {
-
-        }
-    }
+    
 
     public abstract class Movie
     {
@@ -172,5 +70,132 @@ namespace Blockbuster_Lab
         Horror = 2,
         Romance = 3,
         Action = 4
+    }
+
+    public class Blockbuster
+    {
+        public List<Movie> MovieList()
+        {
+            List<Movie> movies = new List<Movie>
+            {
+                new VHS
+                {
+                    Title = "The Fifth Element",
+                    RunTime = 127,
+                    Category = Genre.Action,
+                    Scenes =
+                    {
+                        "LeeLou gets blown Up",
+                        "LeeLou gets reconstructed",
+                        "LeeLou blows up bad planet",
+                        "LeeLou hooks up below her station"
+                    }
+                },
+                new VHS
+                {
+                    Title = "The Wizard of Oz",
+                    RunTime = 112,
+                    Category = Genre.Drama,
+                    Scenes =
+                    {
+                        "Dorothy goes to Oz",
+                        "Dorothy meets some odd folks",
+                        "Dorothy kills a woman",
+                        "Dorothy absconds back to Earth"
+                    }
+                },
+                new VHS
+                {
+                    Title = "Star Wars - A New Hope",
+                    RunTime = 125,
+                    Category = Genre.Action,
+                    Scenes =
+                    {
+                        "Vader tries to kill Leia",
+                        "Luke discovers he's not just a farmer's kid",
+                        "Luke levels up with Yoda",
+                        "Luke blows up Vader's toy"
+                    }
+                },
+                new DVD
+                {
+                    Title = "Inception",
+                    RunTime = 162,
+                    Category = Genre.Drama,
+                    Scenes =
+                    {
+                        "Cobb and crew hacks somebodies brain",
+                        "Cobb goes deeper into the brain",
+                        "Cobb goes deeper into the brain... again",
+                        "Cobb goes super deep and comes back... maybe"
+                    }
+                },
+                new DVD
+                {
+                    Title = "Alita, Battle Angel",
+                    RunTime = 122,
+                    Category = Genre.Drama,
+                    Scenes =
+                    {
+                        "Doc finds cyborg core in dump",
+                        "Doc rebuilds Alita with spare parts",
+                        "Alita trades up for better body",
+                        "Alita joins ultraviolent racing for chance to kill someone who doesn't race"
+                    }
+                },
+                new DVD
+                {
+                    Title = "Ender's Game",
+                    RunTime = 114,
+                    Category = Genre.Drama,
+                    Scenes =
+                    {
+                        "Ender gets recruited",
+                        "Ender leads teams to wins",
+                        "Ender forms an elite team for a different game",
+                        "Ender unknowingly commits genocide"
+                    }
+                }
+            };
+            return movies;
+        }
+
+        public void PrintMovies()
+        {
+            int x = 1;
+
+            foreach (Movie movie in MovieList())
+            {
+                Console.WriteLine($"{x}: {movie.Title}");
+            }
+        }
+
+        public Movie CheckOut()
+        {
+            int userChoice;
+                        
+            Console.WriteLine("Welcome to GC Blockbuster!");
+            PrintMovies();
+            Console.Write("Please Select a Movie from the List: ");
+
+            do
+            {
+                if (int.TryParse(Console.ReadLine(), out int userInput) && userInput > 0 && userInput <= MovieList().Count + 1)
+                {
+                    userChoice = userInput;
+                    break;
+                }
+                else Console.Write("Input Not Recognized. Please try again: ");
+            } while (true);
+
+            Movie currentMovie = MovieList()[userChoice - 1];
+
+            Console.WriteLine();
+            Console.WriteLine($"Title: {currentMovie.Title}");
+            Console.WriteLine($"Category: {currentMovie.Category}");
+            Console.WriteLine($"Runtime: {currentMovie.RunTime}");
+
+            return currentMovie;
+        }
     }
 }
